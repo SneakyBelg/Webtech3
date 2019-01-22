20e2e5c4ec4ea520db5c3e0096cdcda221c4d8ef
SneakyBelg <43534278+SneakyBelg@users.noreply.github.com>
2019-01-22 15:15:25 +0100

    Database connection insert added

--- ExpressOpgave/HerexamenInschrijvingSite/server.js
+++ ExpressOpgave/HerexamenInschrijvingSite/server.js
@@ -3,17 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
-var db
+var db;
 
-MongoClient.connect('mongodb://webtechExamen:administrator1@ds163694.mlab.com:63694/inhaalexamens', (err, client) => {
+MongoClient.connect('mongodb://baseuser:baseuser1@ds143542.mlab.com:43542/examen', (err, client) => {
   if (err) return console.log(err)
-  db = client.db('inhaalexamens')
+  db = client.db('examen')
   app.listen(3000, () => {
     console.log('listening on 3000')
   })
 })
 
-  app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -22,17 +22,18 @@ app.get('/', (req, res) => {
 	res.render('create.ejs', {result: null});
 })
 
-app.post('/search', (req, res) => {
-    
-    naam = req.body.naam;
-    examen = req.body.examen;
-    reden = req.body.reden;
+app.post('/addAanvraag', (req, res) => {
     datumAanvraag = Date.now;
-	
-	db.collection('inhaalAanvragen').insertOne(naam, examen, reden, datumAanvraag, (err, result) => {
-        if (err) return console.log(err)
-    
-        console.log('saved to database')
-        res.redirect('/')
-    })
+    console.log(req.body);
+    if (db.collection('inhaal').find(req.body)){
+        console.log('Already exists');
+    }
+    else{
+        db.collection('inhaal').insertOne(req.body, (err, result) => {
+            if (err) return console.log(err)
+        
+            console.log('saved to database')
+            res.redirect('/')
+        })
+    }
 })
